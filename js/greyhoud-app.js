649d1e4a1455a46892e41e7ab0cfac89bab2cb16
sheenaravary <MsFilth@Sheenas-MacBook-Pro.local>
2014-01-28 20:00:13 -0500

    Fixed video / audio of video

--- js/greyhoud-app.js
+++ js/greyhoud-app.js
@@ -21,3 +21,16 @@ $items.filter(':not([data-state="current"])').hide();
 },4300);
 
 
+var $videoDialog = $('dialog');
+dialogPolyfill.registerDialog($videoDialog.get(0));
+
+$('.btn-open').on('click', function () {
+	$videoDialog.children('.video-open').html('<iframe src="http://player.vimeo.com/video/81434464" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+	$videoDialog.get(0).showModal();
+	$videoDialog.css('width', '60%');
+});
+
+$('.btn-close').on('click', function () {
+	$videoDialog.get(0).close('');
+	$videoDialog.children('.video-open').html('');
+});
