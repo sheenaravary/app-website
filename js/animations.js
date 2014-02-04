51709472a8760fabb654b287f6452c53c543f844
sheenaravary <MsFilth@Sheenas-MacBook-Pro.local>
2014-02-03 20:18:01 -0500

    added waypoint.js

--- js/animations.js
+++ js/animations.js
@@ -1,8 +1,5 @@
 var $trips = $('.trips img');
 
-$('.trips-section').waypoint(function () {
-	$trips.eq(0).addClass('js-ship-delay-1');
-	$trips.eq(3).addClass('js-ship-delay-2');
-	$trips.eq(2).addClass('js-ship-delay-3');
+$('.trips-section').waypoints(function () {
 	$trips.addClass('js-trips-animate');
-}, { offset: '60%' });
+}, { offset: '40%' });
