eceb0fe0445a6431fbf12e70d865413e24c807da
sheenaravary <MsFilth@Sheenas-MacBook-Pro.local>
2014-02-11 07:51:46 -0500

    deleted not needed javascript

--- js/testimonials.js
+++ js/testimonials.js
@@ -5,15 +5,11 @@ $random.load(file);
 
 
 var $dinoData = $.ajax('testimonials.json', {
-		dataType: 'json' //force jquery to reconize JSON and parse the data
+		dataType: 'json' 
 });
 
 $testimonialsData.done(function (data) {
-//triggered when the dino data has finished downloading
-// and i ready to be used
 
-// calculate the random number based on how many items 
-// are in the JSON file
 var rand = Math.round(Math.random() * (data.length - 1) );	
 console.log( data[rand] ); 
 	
